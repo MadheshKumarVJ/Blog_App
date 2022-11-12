727734685860f9feb5d108f13f4f59bf10c12060
MadheshKumarVJ <vjmadhesh@testpress.in>
2022-11-12 12:27:21 +0530

    Support sharing post by email

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,3 +126,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "neomaddy104@gmail.com"
+EMAIL_HOST_PASSWORD = "gkiizqlykfgoowqx"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
