3326a78b6dc6b6df8943f840934bbefa9a76f8ff
MadheshKumarVJ <vjmadhesh@testpress.in>
2022-11-16 16:43:55 +0530

    Refactor list view by using generic view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 
