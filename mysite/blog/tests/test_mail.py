1d386993ba582fc16894bcf303f664d06a432c3b
MadheshKumarVJ <vjmadhesh@testpress.in>
2022-11-21 11:25:42 +0530

    Refactor : Email support

--- mysite/blog/tests/test_mail.py
+++ mysite/blog/tests/test_mail.py
@@ -1,24 +1,13 @@
-from django.core import mail
 from django.test import TestCase
-from blog.tests.test_ModelMixinTestCase import ModelMixinTestCase
-
-
-class EmailTest(TestCase):
-    def test_send_email(self):
-        mail.send_mail(
-            "Subject here",
-            "Here is the message.",
-            "from@example.com",
-            ["to@example.com"],
-            fail_silently=False,
-        )
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject, "Subject here")
+from blog.tests.test_modelmixintestcase import ModelMixinTestCase
+from django.urls import reverse
 
 
 class Test_emailTemplate(ModelMixinTestCase, TestCase):
     def test_post_share_template_used(self):
+        self.post_share_url = reverse(
+            "blog:post_share", args=[self.published_post.id]
+        )
         response = self.client.get(self.post_share_url)
 
-        self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, "blog/post/share.html")
