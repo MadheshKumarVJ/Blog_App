7f5402987ea62b351e484979f53433f65d6f7b0e
MadheshKumarVJ <112080679+MadheshKumarVJ@users.noreply.github.com>
2022-11-30 16:45:11 +0530

    Add a sitemap to the blog site (#18)

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+SITE_ID = 2
 # Application definition
 
 INSTALLED_APPS = [
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "taggit",
+    "django.contrib.sites",
+    "django.contrib.sitemaps",
 ]
 
 MIDDLEWARE = [
